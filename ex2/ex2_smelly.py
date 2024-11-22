69aa3ad063aaef9183c4675561539c3cc15fcf81
waiwenc <waiwen.chan.2022@scis.smu.edu.sg>
2024-11-22 15:42:03 +0800

    refactored ex2_smelly.py and ex2_unit_tests.py

--- ex2/ex2_smelly.py
+++ ex2/ex2_smelly.py
@@ -1,13 +1,26 @@
 class Shape:
-    def calculate_area(self, shape_type, **kwargs):
-        if shape_type == 'circle':
-            radius = kwargs.get('radius')
-            return 3.14 * radius * radius
-        elif shape_type == 'rectangle':
-            length = kwargs.get('length')
-            width = kwargs.get('width')
-            return length * width
-        elif shape_type == 'triangle':
-            base = kwargs.get('base')
-            height = kwargs.get('height')
-            return 0.5 * base * height
+    def calculate_area(self, **kwargs):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return 3.14 * self.radius * self.radius
+
+class Rectangle (Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
